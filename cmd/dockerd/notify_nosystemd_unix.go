20f5dabe22636ff36d18f5761882b88fe42549dd
Enrico Weigelt, metux IT consult <info@metux.net>
2021-05-26 09:40:59 +0200

    (submitted v2) cmd: dockerd: make systemd notification optional
    systemd service notification is not generic to all linux systems,
    only useful on those really running systemd (Lennartix platforms),
    but it introduces dependencies that aren't desired on class Linux targets.
    
    For freebsd target we already have dummy functions that we can use on
    classic Linux. Restructuring in a way that these are used on freebsd
    and on linux when 'nosystemd' tag is set.
    
    Opting out via 'nosystemd' tag, so downstreams dont change suddenly,
    w/o setting any additional tags.
    
    changes v2: renamed tag 'nosystemd' to 'no_systemd'
    
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- cmd/dockerd/notify_nosystemd_unix.go
+++ cmd/dockerd/notify_nosystemd_unix.go
@@ -1,6 +1,6 @@
-package main
+// +build no_systemd freebsd
 
-import systemdDaemon "github.com/coreos/go-systemd/v22/daemon"
+package main
 
 // preNotifyReady sends a message to the host when the API is active, but before the daemon is
 func preNotifyReady() {
@@ -8,11 +8,8 @@ func preNotifyReady() {
 
 // notifyReady sends a message to the host when the server is ready to be used
 func notifyReady() {
-	// Tell the init daemon we are accepting requests
-	go systemdDaemon.SdNotify(false, systemdDaemon.SdNotifyReady)
 }
 
 // notifyStopping sends a message to the host when the server is shutting down
 func notifyStopping() {
-	go systemdDaemon.SdNotify(false, systemdDaemon.SdNotifyStopping)
 }
--- cmd/dockerd/notify_systemd_unix.go
+++ cmd/dockerd/notify_systemd_unix.go
@@ -1,4 +1,8 @@
+// +build !no_systemd,!freebsd
+
 package main
+
+import systemdDaemon "github.com/coreos/go-systemd/v22/daemon"
 
 // preNotifyReady sends a message to the host when the API is active, but before the daemon is
 func preNotifyReady() {
@@ -6,8 +10,11 @@ func preNotifyReady() {
 
 // notifyReady sends a message to the host when the server is ready to be used
 func notifyReady() {
+	// Tell the init daemon we are accepting requests
+	go systemdDaemon.SdNotify(false, systemdDaemon.SdNotifyReady)
 }
 
 // notifyStopping sends a message to the host when the server is shutting down
 func notifyStopping() {
+	go systemdDaemon.SdNotify(false, systemdDaemon.SdNotifyStopping)
 }
